15013b619dd180b65719a8e7243b636361bccc16
Adam Van Ymeren <adam@vany.ca>
2024-12-20 12:08:48 -0800

    soem logs

--- src/foam/nanos/auth/oidc/OIDCWebAgent.java
+++ src/foam/nanos/auth/oidc/OIDCWebAgent.java
@@ -46,6 +46,7 @@ public class OIDCWebAgent implements WebAgent {
                 return;
             }
 
+            logger.info("redirect is" + redirectURI.toString());
             // Exchange authorization code for tokens
             String token = provider.getTokenForCode(x, code, redirectURI != null ? redirectURI : req.getRequestURL().toString());
             if (token == null) {
